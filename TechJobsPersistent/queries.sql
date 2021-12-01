1ece71ce2c7b0d971869c427b13d33b5fdb15878
Kiaran <klearyire@gmail.com>
2021-12-01 14:06:38 -0500

    Initial migrations performed, DB is now set up

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,5 +1,24 @@
 --Part 1
 
+--No idea what I'm supposed to do here, just list the columns and datatypes? I've also 
+--included the simple SQL query, just in case.
+
+SELECT 
+TABLE_CATALOG,
+TABLE_SCHEMA,
+TABLE_NAME, 
+COLUMN_NAME, 
+DATA_TYPE 
+FROM INFORMATION_SCHEMA.COLUMNS
+where TABLE_NAME = 'jobs'
+
+--Returns:
+--int Id
+--longtext Name
+--int EmployerId
+
+
+
 --Part 2
 
 --Part 3
